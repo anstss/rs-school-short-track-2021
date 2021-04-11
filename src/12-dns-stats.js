6fd86a27631bc25a57abe1817c94cf1da75a139d
anstss <anastasiya31017@gmail.com>
2021-04-11 17:23:11 +0300

    feat: add solution for 12-dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,58 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const arrayDomains = domains;
+  for (let i = 0; i < domains.length; i++) {
+    arrayDomains[i] = arrayDomains[i].split('.');
+  }
+  for (let i = 0; i < arrayDomains.length; i++) {
+    for (let j = 0; j < arrayDomains[i].length; j++) {
+      arrayDomains[i][j] = `.${arrayDomains[i][j]}`;
+    }
+  }
+
+  const countNoShadow = {}; // special for eslint №3
+  let tmp = '';
+  let j = 0;
+
+  const noLoopFunction = function (i) {
+    arrayDomains[i].reverse().reduce((count, dom) => {
+      const noParamReassign = count; // special for eslint №1
+      if (!noParamReassign[dom]) {
+        if (tmp === '') {
+          noParamReassign[dom] = 1;
+          tmp = dom;
+          j++;
+        } else {
+          if (!tmp.includes(dom)) {
+            tmp = `${tmp}${dom}`;
+          }
+          if (!count[tmp]) {
+            noParamReassign[tmp] = 1;
+            j++;
+          } else {
+            noParamReassign[tmp]++;
+            j++;
+          }
+        }
+      } else {
+        noParamReassign[dom]++;
+        tmp = `${tmp}${dom}`;
+        j++;
+      }
+      if (j >= arrayDomains[i].length) {
+        tmp = '';
+        j = 0;
+      }
+      return count;
+    }, countNoShadow);
+  };
+
+  for (let i = 0; i < arrayDomains.length; i++) {
+    noLoopFunction(i); // special for eslint №2
+  }
+  return countNoShadow;
 }
 
 module.exports = getDNSStats;
